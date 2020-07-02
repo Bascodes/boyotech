d59318e8c7edfa8f62b110c82bc5b40cf34bcd98
Victor Eyo <victor.eyo@gmail.com>
2020-07-02 00:40:49 -1200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".card-blocks").hover(
+    function () {
+      $(this).animate({ backgroundColor: "#f5f5f5" }, 400);
+    },
+    function () {
+      $(this).animate({ backgroundColor: "#000" }, 400);
+    }
+  );
+});
