09a9d4fdeb6cd43611190be9843f9502b8b267a5
Victor Eyo <victor.eyo@gmail.com>
2020-07-02 23:53:58 -1200

    added parallax and aos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//$(document).ready(function () {
+//$(".card-blocks").hover(
+//function () {
+//$(this).animate({ backgroundColor: "#f5f5f5" }, 400);
+// },
+// function () {
+//  $(this).animate({ backgroundColor: "#000" }, 400);
+// }
+//);
+//});
+
 $(document).ready(function () {
-  $(".card-blocks").hover(
-    function () {
-      $(this).animate({ backgroundColor: "#f5f5f5" }, 400);
-    },
-    function () {
-      $(this).animate({ backgroundColor: "#000" }, 400);
-    }
-  );
+  $(".parallax").parallax();
 });
